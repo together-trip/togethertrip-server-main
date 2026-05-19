da4c64bc30b9bd5ff033759b59b44c4ca11cad44
red-sprout <jjw05015@gmail.com>
2026-05-20 02:09:00 +0900

    feat: 회원가입/로그인 통합 처리

---
+++ src/main/kotlin/com/togethertrip/main/auth/controller/AuthController.kt
@@ -0,0 +1,26 @@
+package com.togethertrip.main.auth.controller
+
+import com.togethertrip.main.auth.dto.KakaoLoginRequest
+import com.togethertrip.main.auth.dto.TokenResponse
+import com.togethertrip.main.auth.service.AuthService
+import com.togethertrip.main.global.response.ApiResponse
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/auth")
+class AuthController(
+    private val authService: AuthService,
+) {
+
+    @PostMapping("/oauth/kakao")
+    fun loginWithKakao(
+        @RequestBody request: KakaoLoginRequest,
+    ): ApiResponse<TokenResponse> {
+        return ApiResponse.success(
+            authService.loginWithKakao(request)
+        )
+    }
+}
